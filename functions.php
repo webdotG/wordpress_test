2565210338a9e651efa405136365e634ebb21140
dotG <65806968+webdotG@users.noreply.github.com>
2021-07-02 22:54:07 +0300

    УДАЛИЛ ФИЛЬТР-удаление заголовка из пагинации

--- functions.php
+++ functions.php
@@ -53,17 +53,7 @@ function theme_register_nav_menu() {
     	global $post;
     	return '<a href="'. get_permalink($post) . '">Читать дальше...</a>';
     } /*настройка для статьи добавляет ссылку на читать дальше---wp-kama.ru/function/the_excerpt*/
-
-    // удаляет H2 из шаблона пагинации
-    add_filter('navigation_markup_template', 'my_navigation_template', 10, 2 );
-    function my_navigation_template( $template, $class ){
-    	/*
-    	Вид базового шаблона:
-    	<nav class="navigation %1$s" role="navigation">
-    		<h2 class="screen-reader-text">%2$s</h2>
-    		<div class="nav-links">%3$s</div>
-    	</nav>
-    	*/
+    
 }
 
    
